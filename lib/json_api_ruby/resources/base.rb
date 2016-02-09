4949e838a6dd2661b33841f6107cd73bb75f26a0
Ben Bean <bbean86@gmail.com>
2016-02-09 15:20:24 -0600

    Refactor to reduce duplication and update version and changelog

--- lib/json_api_ruby/resources/base.rb
+++ lib/json_api_ruby/resources/base.rb
@@ -54,17 +54,11 @@ module JsonApi
         end
       end
 
-      private
-
       # Traverses fields set on super-class(es) and concatinates them into a
       # single set. Stores the set in `self.class.fields`, leaving super-class
       # `fields` sets untouched.
       def fields_array(klass = self.class)
-        fields_list = Array(self.class.fields)
-        not_in_list = Array(klass.fields).reject do |field|
-          fields_list.include?(field)
-        end
-        not_in_list.each { |field| fields_list << field }
+        fields_list = concat_list(self.class.fields, klass.fields)
         unless klass.superclass == Resource
           return fields_array(klass.superclass)
         end
@@ -75,15 +69,24 @@ module JsonApi
       # into a single set. Stores the set in `self.class.relationships`,
       # leaving super-class `relationships` sets untouched.
       def relationships_array(klass = self.class)
-        rel_list = Array(self.class.relationships)
-        not_in_list = Array(klass.relationships).reject do |rel|
-          rel_list.include?(rel)
-        end
-        not_in_list.each { |rel| rel_list << rel }
+        rel_list = concat_list(self.class.relationships, klass.relationships)
         unless klass.superclass == Resource
           return relationships_array(klass.superclass)
         end
         rel_list
+      end
+
+      private
+
+      # Adds elements of the `concat_list` to the `target_list` if they are
+      # not already present.
+      def concat_list(target_list, concat_list)
+        list = Array(target_list)
+        not_in_list = Array(concat_list).reject do |item|
+          list.include?(item)
+        end
+        not_in_list.each { |item| list << item }
+        list
       end
     end
 
