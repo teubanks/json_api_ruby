14fe7f6e73cec01f6cd839ac9b2e5839473b0402
Tracey Eubanks <tracey@bypassmobile.com>
2016-02-23 12:48:00 -0600

    consolidated serializer logic

--- lib/json_api_ruby/serializer.rb
+++ lib/json_api_ruby/serializer.rb
@@ -14,86 +14,67 @@ module JsonApi
     if !object.nil? && object.respond_to?(:to_a)
       serializer = CollectionSerializer.new(object, options)
     else
-      serializer = Serializer.new(object, options)
+      serializer = ObjectSerializer.new(object, options)
     end
     serializer.to_hash
   end
 
   class Serializer
+    attr_reader :object
+
     def initialize(object, options)
       @meta           = options.fetch('meta', Hash.new).stringify_keys
       @object         = object
       @includes       = options.fetch('include', [])
-      resource_name   = "#{@object.class.to_s.underscore}_resource".classify
-      @resource_class = options.fetch('resource_class', resource_name)
-    end
-
-    def to_hash
-      if @object.nil?
-        serialized = { 'data' => nil }
-      else
-        resource_klass = Resources::Discovery.resource_for_name(@object, resource_class: @resource_class)
-        resource       = resource_klass.new(@object, include: @includes)
-        serialized     = { 'data' => resource.to_hash }
-        relationships  = resource.relationships
-        included_data  = assemble_included_data(relationships)
-
-        if included_data.present?
-          included_data.uniq! do |inc_data|
-            inc_data['id'] + inc_data['type']
-          end
-          serialized['included'] = included_data
-        end
-      end
-
-      serialized['meta'] = @meta if @meta.present?
-      serialized
-    end
-
-    def assemble_included_data(relationships)
-      relationships.flat_map do |relationship|
-        next if relationship.resources.blank?
-        relationship.resources.map(&:to_hash)
-      end.compact
-    end
-  end
-
-  class CollectionSerializer
-    def initialize(objects, options = {})
-      @meta           = options.fetch('meta', Hash.new).stringify_keys
-      @objects        = objects
-      @includes       = options.fetch('include', [])
       @resource_class = options.fetch('resource_class', nil)
     end
 
+    def resource(object)
+      resource_name  = "#{object.class.to_s.underscore}_resource".classify
+      klass_name     = @resource_class || resource_name
+      resource_klass = Resources::Discovery.resource_for_name(object, resource_class: klass_name)
+      resource_klass.new(object, include: @includes)
+    end
+
+    def assemble_included_data(included_resources)
+      included_resources.uniq { |rel| rel.id + rel.type }.map(&:to_hash)
+    end
+  end
+
+  class ObjectSerializer < Serializer
+    def to_hash
+      included_resources = []
+      if @object.nil?
+        serialized = { 'data' => nil }
+      else
+        object_resource = resource(@object)
+        serialized = { 'data' => object_resource.to_hash }
+        included_resources += object_resource.relationships.select {|rel| rel.included?}.flat_map {|rel| rel.resources }
+      end
+
+      serialized['included'] = assemble_included_data(included_resources) if included_resources.present?
+      serialized['meta'] = @meta if @meta.present?
+
+      serialized
+    end
+  end
+
+  class CollectionSerializer < Serializer
     def to_hash
       serialized = {}
       included_resources = []
 
-      data_array = @objects.map do |object|
-        resource_name  = "#{object.class.to_s.underscore}_resource".classify
-        klass_name     = @resource_class || resource_name
-        resource_klass = Resources::Discovery.resource_for_name(object, resource_class: klass_name)
-        resource       = resource_klass.new(object, include: @includes)
-        included_resources += resource.relationships.select {|rel| rel.included?}.flat_map {|rel| rel.resources }
-
-        resource.to_hash
-      end
-
-      included_resources.uniq! do |rel|
-        rel.id + rel.type
+      data_array = Array(@object).map do |object|
+        object_resource = resource(object)
+        included_resources += object_resource.relationships.select {|rel| rel.included?}.flat_map {|rel| rel.resources }
+        object_resource.to_hash
       end
 
       serialized['data'] = data_array
-
-      serialized['meta'] = @meta if @meta.present?
       serialized['included'] = assemble_included_data(included_resources) if included_resources.present?
+      serialized['meta'] = @meta if @meta.present?
 
       serialized
-    end
-
-    def assemble_included_data(included_resources)
-      included_resources.map(&:to_hash)
     end
   end
 end
