92678c40e51d9242b3264042f666c5788cde32be
Tracey Eubanks <tracey@bypassmobile.com>
2016-02-29 17:28:03 -0600

    Filled out specs
    I had written some spec files but hadn't filled them out.

--- lib/json_api_ruby/associations/meta.rb
+++ lib/json_api_ruby/associations/meta.rb
@@ -1,6 +1,12 @@
 module JsonApi
   module Associations
+    UnknownCardinalityError = Class.new(StandardError)
+
     class Meta
+      TO_ONE = :one
+      TO_MANY = :many
+      CARDINALITY = [TO_ONE, TO_MANY]
+
       # The name of this relationship.
       #
       # This name comes from the resource object that defines the
@@ -15,14 +21,21 @@ module JsonApi
 
       attr_reader :explicit_resource_class
 
-      def initialize(name, options)
+      def initialize(name, resource_class: nil, cardinality:)
         @name = name.to_s
-        @cardinality = options.fetch(:cardinality)
-        @explicit_resource_class = options.fetch(:resource_class, nil)
+        validate_cardinality(cardinality)
+        @cardinality = cardinality
+        @explicit_resource_class = resource_class
+      end
+
+      def validate_cardinality(cardinality)
+        unless(CARDINALITY.include?(cardinality))
+          fail UnknownCardinalityError.new
+        end
       end
 
       def build_resources(options)
-        if cardinality == :one
+        if cardinality == TO_ONE
           relationship = ToOne.new(name, options.merge(explicit_resource_class: explicit_resource_class))
         else
           relationship = ToMany.new(name, options.merge(explicit_resource_class: explicit_resource_class))
